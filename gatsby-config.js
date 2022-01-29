b5f0c3e16e82fbde52676a09d2c04e46bf28f961
kalijonn <43421621+kalijonn@users.noreply.github.com>
2022-01-29 18:32:33 +0530

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `gatsby-blog`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `My First Gatsby Site`,
+    siteUrl: `https://kalijonn-gatsby-tut.netlify.app`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
